d705bef54f9ff80db468828204b48fcfd70318df
Rick Cogley <rick.cogley@esolia.co.jp>
2019-01-30 19:32:32 +0900

    Swap ctrl and shift on thumbs back to default
    Signed-off-by: Rick Cogley <rick.cogley@esolia.co.jp>

--- Model01/Model01-Firmware.ino
+++ Model01/Model01-Firmware.ino
@@ -225,14 +225,14 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Minus,
    Key_Tab,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftAlt,
-   Key_LeftShift, Key_Backspace, Key_LeftGui, Key_LeftControl,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_TOGGLEIME),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backspace,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightGui,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightShift,
-   Key_RightControl, Key_RightAlt, Key_Spacebar, Key_RightShift,
+   Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
 #else
